677222bafd3cc65d750b3569a4d1204f7cf44bcd
mikespub <mikespub@gmail.com>
2019-12-15 18:16:32 +0100

    btfs auth & users

---
+++ src/btfs/auth.py
@@ -0,0 +1,36 @@
+# CHECKME: use IAP, wrapper or middleware to set authenticated header
+#
+# See also https://github.com/mar10/wsgidav/issues/109
+#
+"""
+Taken from 
+  http://appengine-cookbook.appspot.com/recipe/restrict-application-to-an-authorized-set-of-users/
+"""
+from __future__ import absolute_import
+from builtins import object
+
+from google.appengine.ext import db
+import logging
+
+try:
+    from google.appengine.api import users
+except:
+    from . import users
+
+    # TODO: make configurable
+    users.AUTH_URL = '/auth/'
+
+
+class AuthorizedUser(db.Model):
+    """Represents authorized users in the datastore."""
+    user = db.UserProperty()
+    canWrite = db.BooleanProperty(default=True)
+
+
+def findAuthUser(email):
+    """Return AuthorizedUser for `email` or None if not found."""
+    user = users.User(email)
+    auth_user =  AuthorizedUser.gql("where user = :1", user).get()
+    logging.debug("findAuthUser(%r) = %s" % (email, auth_user))
+    return auth_user
+    
---
+++ src/btfs/users.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""The User Python datastore class to be used as a datastore data type."""
+from builtins import str
+from past.builtins import cmp
+from builtins import object
+import os
+
+# TODO: make configurable
+AUTH_URL = '/_ah/'
+LOGIN_URL = AUTH_URL + 'login'
+LOGOUT_URL = AUTH_URL + 'logout'
+
+
+class Error(Exception):
+  """Base User error type."""
+
+
+class UserNotFoundError(Error):
+  """No email argument was specified, and no user is logged in."""
+
+
+class RedirectTooLongError(Error):
+  """The generated redirect URL was too long."""
+
+
+class NotAllowedError(Error):
+  """The requested redirect URL is not allowed."""
+
+
+class User(object):
+  """Provides the email address, nickname, and ID for a user.
+
+  A nickname is a human-readable string that uniquely identifies a Google user,
+  akin to a username. For some users, this nickname is an email address, but for
+  other users, a different nickname is used.
+
+  A user is a Google Accounts user.
+  """
+
+  __user_id = None
+
+  def __init__(self, email=None, _auth_domain=None,
+               _user_id=None, _strict_mode=True):
+    """Constructor.
+
+    Args:
+      email: An optional string of the user's email address. It defaults to
+          the current user's email address.
+
+    Raises:
+      UserNotFoundError: If the user is not logged in and `email` is empty
+    """
+
+    if _auth_domain is None:
+      _auth_domain = os.environ.get('AUTH_DOMAIN')
+    assert _auth_domain
+
+    if email is None:
+      email = os.environ.get('USER_EMAIL', email)
+      _user_id = os.environ.get('USER_ID', _user_id)
+
+    if email is None:
+      email = ''
+
+    if not email and _strict_mode:
+      raise UserNotFoundError
+
+    self.__email = email
+    self.__auth_domain = _auth_domain
+    self.__user_id = _user_id or None
+
+
+  def nickname(self):
+    """Returns the user's nickname.
+
+    The nickname will be a unique, human readable identifier for this user with
+    respect to this application. It will be an email address for some users,
+    and part of the email address for some users.
+
+    Returns:
+      The nickname of the user as a string.
+    """
+    if (self.__email and self.__auth_domain and
+        self.__email.endswith('@' + self.__auth_domain)):
+      suffix_len = len(self.__auth_domain) + 1
+      return self.__email[:-suffix_len]
+    else:
+      return self.__email
+
+  def email(self):
+    """Returns the user's email address."""
+    return self.__email
+
+  def user_id(self):
+    """Obtains the user ID of the user.
+
+    Returns:
+      A permanent unique identifying string or `None`. If the email address was
+      set explicitly, this will return `None`.
+    """
+    return self.__user_id
+
+  def auth_domain(self):
+    """Obtains the user's authentication domain.
+
+    Returns:
+      A string containing the authentication domain. This method is internal and
+      should not be used by client applications.
+    """
+    return self.__auth_domain
+
+  def __str__(self):
+    return str(self.nickname())
+
+  def __repr__(self):
+    values = []
+    if self.__email:
+      values.append("email='%s'" % self.__email)
+    if self.__user_id:
+      values.append("_user_id='%s'" % self.__user_id)
+    return 'users.User(%s)' % ','.join(values)
+
+  def __hash__(self):
+    return hash((self.__email, self.__auth_domain))
+
+  def __cmp__(self, other):
+    if not isinstance(other, User):
+      return NotImplemented
+    return cmp((self.__email, self.__auth_domain),
+               (other.__email, other.__auth_domain))
+
+
+def create_login_url(dest_url=None, _auth_domain=None):
+  """Computes the login URL for redirection.
+
+  Args:
+    dest_url: String that is the desired final destination URL for the user
+        once login is complete. If `dest_url` does not specify a host, the host
+        from the current request is used.
+
+  Returns:
+       Login URL as a string. The login URL will use Google Accounts.
+  """
+  if dest_url:
+    return LOGIN_URL + '?continue=%s' % dest_url
+  return LOGIN_URL
+
+
+def create_logout_url(dest_url, _auth_domain=None):
+  """Computes the logout URL and specified destination URL for the request.
+
+  This function works for Google Accounts applications.
+
+  Args:
+    dest_url: String that is the desired final destination URL for the user
+        after the user has logged out. If `dest_url` does not specify a host,
+        the host from the current request is used.
+
+  Returns:
+    Logout URL as a string.
+  """
+  if dest_url:
+    return LOGOUT_URL + '?continue=%s' % dest_url
+  return LOGOUT_URL
+
+
+def get_current_user():
+  """Retrieves information associated with the user that is making a request.
+
+  Returns:
+
+  """
+  try:
+    return User()
+  except UserNotFoundError:
+    return None
+
+
+def is_current_user_admin():
+  """Specifies whether the user making a request is an application admin.
+
+  Because administrator status is not persisted in the datastore,
+  `is_current_user_admin()` is a separate function rather than a member function
+  of the `User` class. The status only exists for the user making the current
+  request.
+
+  Returns:
+    `True` if the user is an administrator; all other user types return `False`.
+  """
+  return (os.environ.get('USER_IS_ADMIN', '0')) == '1'
+
