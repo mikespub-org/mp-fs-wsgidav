61f6b835ada3a0a6d75e29ab63a1be4e01f556ab
mikespub <github@mikespub.net>
2021-09-27 14:49:29 +0000

    use Firebase SDK v9 in compat mode

--- src/auth.py
+++ src/auth.py
@@ -131,7 +131,7 @@ def user_token():
         FIREBASE_PROJECT_ID=os.environ.get("FIREBASE_PROJECT_ID", "MY_PROJECT_ID"),
         FIREBASE_API_KEY=os.environ.get("FIREBASE_API_KEY", "MY_API_KEY"),
         FIREBASE_ID_TOKEN=sessions.get_cookie_name("id_token"),
-        FIREBASEJS_SDK_VERSION=os.environ.get("FIREBASEJS_SDK_VERSION", "7.18.0"),
-        FIREBASEJS_UI_VERSION=os.environ.get("FIREBASEJS_UI_VERSION", "4.6.1"),
+        FIREBASEJS_SDK_VERSION=os.environ.get("FIREBASEJS_SDK_VERSION", "9.1.0"),
+        FIREBASEJS_UI_VERSION=os.environ.get("FIREBASEJS_UI_VERSION", "5.0.0"),
         PROXY_PREFIX=os.environ.get("PROXY_PREFIX", ""),
     )
