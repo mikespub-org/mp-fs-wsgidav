4b9443a8db6dac892f2132bfccd303dcc71b2887
mikespub <github@mikespub.net>
2021-11-27 18:18:40 +0100

    update requirements

---
+++ make_toc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import re
+
+output = "## Table of Contents ##\n"
+
+with open("README.md", "r") as fp:
+    find = re.compile(r"^(##+)\s+(.+?)\s+#+$")
+    for line in fp:
+        m = find.match(line)
+        if m:
+            if m.group(2) == "Table of Contents":
+                continue
+            output += (
+                "  " * (len(m.group(1)) - 1)
+                + "* ["
+                + m.group(2)
+                + "](#"
+                + m.group(2).lower().replace(",", "").replace(" ", "-")
+                + ")\n"
+            )
+    output += "\n"
+
+with open("toc.md", "w") as fp:
+    fp.write(output)
